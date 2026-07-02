7e1b98cce8f40088bbb9c0ae3f00579884d562bf
fathirpamungkas <74666566+fathirpamungkas@users.noreply.github.com>
2026-07-03 01:16:00 +0700

    Add files via upload

--- PREMIUMM.lua
+++ PREMIUMM.lua
@@ -48,7 +48,7 @@ local senhasValidas = {
         usuario = "PREMIUM", 
         dispositivos = "0", 
         criado = "01/01/2026 H00:00", 
-        ano = 2026, mes = 7, dia = 11, hora = 10, min = 00
+        ano = 2026, mes = 7, dia = 12, hora = 01, min = 20
     },
    -- ====================================================
  ["DJAROTEXP"] = {
