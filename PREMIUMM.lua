14c5b877052ec2a0e1ba7cee1e58ebc808cf88fb
fathirpamungkas <74666566+fathirpamungkas@users.noreply.github.com>
2026-07-08 21:39:25 +0700

    Add files via upload

--- PREMIUMM.lua
+++ PREMIUMM.lua
@@ -65,11 +65,11 @@ local senhasValidas = {
         ano = 2026, mes = 7, dia = 12, hora = 13, min = 00
     },
        -- ====================================================
- ["FORJI"] = {
+ ["MARSEL"] = {
         usuario = "PREMIUM", 
         dispositivos = "0", 
         criado = "01/01/2026 H00:00", 
-        ano = 2026, mes = 7, dia = 6, hora = 23, min = 00
+        ano = 2029, mes = 7, dia = 6, hora = 23, min = 00
     },
   -- =====================================================
 ["ICHTIAR"] = {
