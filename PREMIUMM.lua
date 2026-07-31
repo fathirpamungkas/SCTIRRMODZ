f9e1cf8902ac23e1cfdd78a3a5dfc7cc22d5d7a1
fathirpamungkas <74666566+fathirpamungkas@users.noreply.github.com>
2026-08-01 01:04:48 +0700

    Add files via upload

--- PREMIUMM.lua
+++ PREMIUMM.lua
@@ -23,7 +23,7 @@ local senhasValidas = {
     ["CASPER63"] = {usuario = "PREMIUM", dispositivos = "1", criado = "01/01/2026 H00:00", ano = 2026, mes = 7, dia = 28, hora = 16, min = 31},
     ["HAPIS"] = {usuario = "PREMIUM", dispositivos = "1", criado = "01/01/2026 H00:00", ano = 2026, mes = 7, dia = 28, hora = 15, min = 20},
     ["NOIRBLANC123"] = {usuario = "PREMIUM", dispositivos = "2", criado = "30/06/2026 H19:00", ano = 2026, mes = 8, dia = 1, hora = 23, min = 15},
-    ["ROXIO"] = {usuario = "KELUARGA", dispositivos = "99", criado = "01/01/2026 H00:00", ano = 2027, mes = 1, dia = 1, hora = 00, min = 00}
+    ["DLUCA"] = {usuario = "KELUARGA", dispositivos = "99", criado = "01/01/2026 H00:00", ano = 2027, mes = 1, dia = 1, hora = 00, min = 00}
 }
 -- ======================================================
 -- 👆👆👆👆 PAINEL SENHAS E USUÁRIO 👆👆👆👆
